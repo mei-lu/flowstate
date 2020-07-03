f257d659a9e308941649f83e3b5eea61db8b5d30
Mei <meiluwork@gmail.com>
2020-07-02 21:56:57 -0400

    Serve app with client side routing

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(express.json());
 // Route Middlewares
 app.use('/api/users', authRoute);
 app.use('/api/stats', statsRoute);
-app.use(express.static(path.join(__dirname, 'build')))
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => console.log('Server started~ <3'));
