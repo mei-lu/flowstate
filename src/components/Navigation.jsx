90c32d5c6e235b89a1b86d1167f8c44d6a936650
Mei <meiluwork@gmail.com>
2020-07-18 17:42:32 -0400

    Create component for kanban taskcard

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from '../components/Logo';
 import {Link} from '@reach/router';
 class Navigation extends React.Component {
     render() {
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 class Kanban extends React.Component {
-
+    constructor(props) {
+        super(props);
+    }
 }
 
 export default Kanban;
