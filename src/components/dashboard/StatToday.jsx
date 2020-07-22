acb89d29ea27ef9276b6168b05a78b33d3894730
Mei <meiluwork@gmail.com>
2020-07-22 19:04:40 -0400

    Dashboard styling grid

--- src/components/dashboard/StatToday.jsx
+++ src/components/dashboard/StatToday.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 class StatToday extends React.Component {
     render() {
         return <div className='today-stat'>
-            <h3>Today</h3>
-            <p>+{this.props.currentStreak} Days</p>
-            <p>+{this.props.points} Points</p>
-            <p>+{this.props.focusTime} Mins</p>
+            <h3>Today's Progress</h3>
+            <p>{this.props.points} Contributions</p>
+            <p>{this.props.focusTime} Mins focused</p>
         </div>
     }
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,12 +6,20 @@ import ProfilePic from '../img/ProfilePic.jpg';
 
 class Dashboard extends React.Component{
     render() {
-        return <div>
-            <div><img className='profile-pic' src={ProfilePic} alt=''/></div>
-            <StatToday currentStreak='20' points='10' focusTime='49'/>
-            <StatCardSmall title='Record Streak' data='48 Days'/>
-            <StatCardSmall title='Total Focus' data='3 Days 10 Hrs 30 Mins'/>
-            <h3 class='contribution-count'>293 Stars</h3>
+        return <div className='full-width'>
+            <div className='dashboard-tiles'>
+                <div className='profile-section'>
+                    <img className='profile-pic' src={ProfilePic} alt=''/>
+                    <h1>Hi Mei!</h1>
+                </div>
+                <StatToday className='today-stats-group' points='10' focusTime='49'/>
+                <div className='small-stats-group'>
+                    <StatCardSmall title='Current Streak' data='20 Days'/>
+                    <StatCardSmall title='Record Streak' data='48 Days'/>
+                    <StatCardSmall title='Total Focus' data='3 Days 10 Hrs 30 Mins'/>
+                </div>
+            </div>
+            <h3 class='contribution-count'>293 Contributions</h3>
             <ContributionGraph/>
         </div>
     }
