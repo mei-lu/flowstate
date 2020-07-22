22eaaf23e25dc83dd8b091f0a33dd98b4351eb33
Mei <meiluwork@gmail.com>
2020-07-21 22:27:54 -0400

    Remove useless constructor for build

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 class Kanban extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return <p>Kanban Page!</p>
        }
