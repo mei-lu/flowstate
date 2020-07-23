49934ddf52981bc5ae328c2b547233fbf6fff718
Mei <meiluwork@gmail.com>
2020-07-23 13:11:43 -0400

    Layout changes on dash

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,20 +8,29 @@ class Dashboard extends React.Component{
         return <div className='full-width'>
             <div className='dashboard-tiles'>
                 <div className='profile-section'>
-                    <div className='profile-pic' style={{backgroundImage: `url(${ProfilePic})`}}></div>
-                    {/* <img className='profile-pic' src={ProfilePic} alt=''/> */}
-                    <h1>Hi Mei!</h1>
-                </div>
-                <div className='today-streak'><p className='dashboard-heading'>13 Days</p> Current Streak</div>
-                <div className='today-contribution'><p className='dashboard-heading'>10 Tasks</p>Left Today</div>
-                <div className='small-stats-group'>
+                    <div className='profile-card'>
+                        <div className='profile-pic' style={{backgroundImage: `url(${ProfilePic})`}}></div>
+                        <h1>Hi Mei!</h1>
+                    </div>
+                    <h3>Today's Stats</h3>
+                    <div className='small-stats-group'>
                     <StatCardSmall title='Contributions' data='+10'/>
                     <StatCardSmall title='Record Streak' data='48 Days'/>
                     <StatCardSmall title='Total Focus' data='10 Hrs 30 Mins'/>
                 </div>
+                </div>
+                <div className='today-section'>
+                    <div className='today-card'><p className='dashboard-heading'>13 Days</p> Current Streak</div>
+                    <div className='today-card'><p className='dashboard-heading'>10 Tasks</p>Left Today</div>
+                </div>
+
+                <div className='graph-section'>
+                    <h3 class='contribution-count'>293 contributions this year</h3>
+                    <ContributionGraph/>
+                </div>
             </div>
-            <h3 class='contribution-count'>293 contributions this year</h3>
-            <ContributionGraph/>
+            
+            
         </div>
     }
 }
