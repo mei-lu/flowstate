30df6782079c580deb83cec24c1091b19114c457
Mei <meiluwork@gmail.com>
2020-08-04 17:25:35 -0400

    Split css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Pomodoro from './pages/Pomodoro';
+import Pomodoro from './components/pomodoro/Pomodoro';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Navigation from './components/Navigation';
+import Navigation from './components/sidebar/Navigation';
 import Landing from './pages/Landing';
 import Dashboard from './pages/Dashboard';
 import Kanban from './pages/Kanban';
+import Schedule from './pages/Schedule';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navigation />
         <Switch>
+        <Route exact path='/' component={Landing}/>
           <div className="Contents">
-          <Route exact path='/' component={Landing}/>
+          <Navigation />
           <Route path='/tasks' component={Kanban}/>
+          <Route path='/schedule' component={Schedule}/>
           <Route path='/pomodoro' component={Pomodoro}/>
           <Route path='/dashboard' component={Dashboard}/>
           <Route path='/login' component={Login}/>
--- src/components/sidebar/Navigation.jsx
+++ src/components/sidebar/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {withRouter, Link} from 'react-router-dom';
-import Logo from './Logo';
+import Logo from '../Logo';
 class Navigation extends React.Component {
     render() {
         return <div className='navigation'>
@@ -8,6 +8,7 @@ class Navigation extends React.Component {
             <div className='nav-labels'>
                 <Link to='/dashboard'>Dashboard</Link>
                 <Link to='/tasks'>Tasks</Link>
+                <Link to='/schedule'>Schedule</Link>
                 <Link to='/pomodoro'>Timer</Link>
                 <Link to='/login'>Log In</Link>
             </div>
