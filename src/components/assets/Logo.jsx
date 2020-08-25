309716909b059af5b9ba53e49d778ca8541531a5
Mei <meiluwork@gmail.com>
2020-08-25 18:06:41 -0400

    Change favicon and add logo

--- src/components/assets/Logo.jsx
+++ src/components/assets/Logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import LogoImg from '../img/logo.svg'
+import LogoImg from '../../img/logo.svg';
 import { useHistory } from 'react-router-dom';
 
 function Logo() {
     const history = useHistory();
 
     return <div className='logo' onClick={() => history.push('/')}>
-        {/* <img className='logo-img' src={LogoImg} alt=''/> */}
+        <img className='logo-img' src={LogoImg} alt=''/>
         Flowstate
     </div>
 }
