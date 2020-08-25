49fe58a09f4210bb594181833af2458aa7eb6dca
Mei <meiluwork@gmail.com>
2020-08-25 00:36:30 -0400

    Tasks page styling

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import TaskCard from '../components/tasks/TaskCard';
 
 function Tasks() {
-    return <div className='tasks-wrapper'>
-        <TaskCard due='Sept 12, 2020' title='Tidy up' description='Organize work space'/>
+    return( 
+    <div>
+        <div className='column'>
+            <div className='column-name'>
+                <p>Priority</p>
+                <p>...</p>
+            </div>
+            <TaskCard due='Sept 12, 2020' title='Tidy up' description='Organize work space'/>
+            <TaskCard due='Sept 12, 2020' title='Tidy up' description='Organize work space'/>
+            <TaskCard due='Sept 12, 2020' title='Tidy up' description='Organize work space'/>
+        </div>
     </div>
+    );
 }
 export default Tasks;
